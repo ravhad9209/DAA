9f37e84b8725095eb60e104d4cb2a0071b59c14e
Avhad Roshan Vials <167279797+ravhad9209@users.noreply.github.com>
2025-11-11 02:27:05 +0000

    11/11/25

--- 1_fibonacci_numbers.py
+++ 1_fibonacci_numbers.py
@@ -1,32 +1,36 @@
+# Recursive Fibonacci with step count
+steps = 0  # Global variable to count steps
+
 def recursive_fibonacci(n):
-    # Base cases: F(0) = 0, F(1) = 1
+    global steps
+    steps += 1  # Count each function call
     if n <= 1:
         return n
     else:
-        # Recursive case: F(n) = F(n-1) + F(n-2)
-        return recursive_fibonacci(n-1) + recursive_fibonacci(n-2)
+        return recursive_fibonacci(n - 1) + recursive_fibonacci(n - 2)
+
+# Non-recursive (iterative) Fibonacci with step count
+def non_recursive_fibonacci(n):
+    steps = 0
+    a, b = 0, 1
+    print("Fibonacci sequence (Iterative):")
+    for i in range(n):
+        print(a)
+        steps += 1
+        a, b = b, a + b
+    print("Total steps (iterations):", steps)
 
 # Main program
 if __name__ == "__main__":
-    n = int(input("Enter Your number :"))
-    print("Fibonacci sequence:")
+    n = int(input("Enter number of terms: "))
+
+    # Recursive version
+    steps = 0  # Reset step counter
+    print("\nFibonacci sequence (Recursive):")
     for i in range(n):
         print(recursive_fibonacci(i))
-    
+    print("Total recursive steps (function calls):", steps)
 
-# def non_recursive_fibonacci(n):
-#     first = 0
-#     second = 1
-
-#     print("Fibonacci sequence:")
-#     for i in range(n):
-#         print(first)
-#         # Update the two previous numbers
-#         next_num = first + second
-#         first = second
-#         second = next_num
-
-# # Main program
-# if __name__ == "__main__":
-#     n = int(input("Enter the number of terms: "))
-#     non_recursive_fibonacci(n)
+    # Iterative version
+    print("_"*30)
+    non_recursive_fibonacci(n)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,9 @@
+def non_recursive_fibonacci(n):
+#     steps = 0
+#     a, b = 0, 1
+#     print("Fibonacci sequence (Iterative):")
+#     for i in range(n):
+#         print(a)
+#         steps += 1
+#         a, b = b, a + b
+#     print("Total steps (iterations):", steps)
